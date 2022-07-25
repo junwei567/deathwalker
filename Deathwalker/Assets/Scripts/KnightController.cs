c916e9e848ba5a3419119ce99e517e00c7b68f11
ChungZhiYou <73518250+ChungZhiYou@users.noreply.github.com>
2022-07-25 18:45:04 +0800

    knight dashing fixed

--- Deathwalker/Assets/Scripts/KnightController.cs
+++ Deathwalker/Assets/Scripts/KnightController.cs
@@ -13,7 +13,10 @@ public class KnightController : Movement
     public float attackRadius;
     private Animator anim;
     public LayerMask whatIsPlayer;
-    public float dashSpeed = 10.0f;
+    public float dashSpeed = 1.2f;
+    private int step = 0;
+    private float target_x;
+    private float target_y;
 
     // Start is called before the first frame update
     protected override void Start()
@@ -37,35 +40,63 @@ public class KnightController : Movement
         if (inRange && !lunging) {
             // If cooldown is over, shoot
             if (Time.time - lastUsed > cooldown) {
-                // Debug.Log("Lunging");
+                Debug.Log("Lunging");
                 lunging = true;
                 anim.SetBool("inAttackRange", inRange);
-                Lunge(x, y); 
+                target_x = x;
+                target_y = y;
+                // Lunge(x, y); 
             }
+        }
+        if (lunging){
+            Debug.Log(step);
+            if (step >= 100 && step <= 150){
+                anim.SetBool("inAttackRange", false);
+                UpdateMovement(new Vector3(target_x, target_y, 0).normalized * 7.0f);
+            }
+            else if (step >= 250){
+                lunging = false;
+                lastUsed = Time.time;
+                step = 0;
+            }
+            step += 1;
         }
         // If player is not in shooting range
         if (!lunging) {
             // Debug.Log(lunged);
-            UpdateMovement(new Vector3(x,y,0));
+            UpdateMovement(new Vector3(x,y,0).normalized * 0.7f);
         }
     }
-    void Lunge(float target_x,float target_y)
-    {   
-        StartCoroutine(countdownToLunge(target_x,target_y));
-    }
-    IEnumerator countdownToLunge(float target_x, float target_y) {
-        yield return new WaitForSeconds(0.5f);
-        anim.SetBool("inAttackRange", false);
-        int step = 0;
-        while (step < 4){
-            UpdateMovement(new Vector3(target_x, target_y, 0).normalized * dashSpeed);
-            Debug.Log(new Vector3(target_x, target_y, 0).normalized * dashSpeed);
-            step += 1;
-        }
-        yield return new WaitForSeconds(0.5f);
-        // Start shooting after waiting for an initial 1.5 seconds
-        lunging = false;
-        lastUsed = Time.time;
-    }
+    // void Lunge(float target_x,float target_y)
+    // {   
+    //     // StartCoroutine(countdownToLunge(target_x,target_y));
+    //     anim.SetBool("inAttackRange", false);
+    //     int step = 0;
+    //     while (step < 5){
+    //         UpdateMovement(new Vector3(target_x, target_y, 0).normalized * dashSpeed);
+    //         Debug.Log(new Vector3(target_x, target_y, 0).normalized * dashSpeed);
+    //         step += 1;
+    //         yield return new WaitForSeconds(0.003f);
+    //     }
+    //     yield return new WaitForSeconds(0.25f);
+    //     // Start shooting after waiting for an initial 1.5 seconds
+    //     lunging = false;
+    //     lastUsed = Time.time;
+    // }
+    // IEnumerator countdownToLunge(float target_x, float target_y) {
+    //     yield return new WaitForSeconds(0.25f);
+    //     anim.SetBool("inAttackRange", false);
+    //     int step = 0;
+    //     while (step < 5){
+    //         UpdateMovement(new Vector3(target_x, target_y, 0).normalized * dashSpeed);
+    //         Debug.Log(new Vector3(target_x, target_y, 0).normalized * dashSpeed);
+    //         step += 1;
+    //         yield return new WaitForSeconds(0.003f);
+    //     }
+    //     yield return new WaitForSeconds(0.25f);
+    //     // Start shooting after waiting for an initial 1.5 seconds
+    //     lunging = false;
+    //     lastUsed = Time.time;
+    // }
 }
 
