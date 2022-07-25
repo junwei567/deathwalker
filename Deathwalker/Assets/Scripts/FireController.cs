d0b95a92d817453c2b042a206ff149455cbec9ce
Marcus Ho Jw <marcushojww@gmail.com>
2022-07-25 15:40:48 +0800

    archer animation, mage creation + animation for spell

---
+++ Deathwalker/Assets/Scripts/FireController.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireController : MonoBehaviour
+{
+    private SpriteRenderer fireSprite;
+    private Animator fireAnimator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        fireSprite = GetComponent<SpriteRenderer>();
+        fireAnimator = GetComponent<Animator>();
+        // Change fire alpha channel to 0.5
+        fireSprite.color = new Color(1f,1f,1f,0.5f);
+        // Change fire scale to 0.5
+        this.gameObject.transform.localScale = new Vector3(0.5f,0.5f,0.5f);
+        StartCoroutine(StartFire(fireSprite));
+    }
+
+    IEnumerator StartFire(SpriteRenderer fireSprite)
+    {
+        yield return new WaitForSeconds(0.8f);
+        this.gameObject.transform.localScale = new Vector3(1f,1f,1f);
+        // Stop blinking animation
+        fireAnimator.SetBool("castTimeOver", true);
+        // Set alpha channels back to default
+        fireSprite.color = new Color(1f,1f,1f,1f);
+        StartCoroutine(EndFire());
+    }
+    IEnumerator EndFire()
+    {
+        yield return new WaitForSeconds(2.0f);
+        // Start the end fire animation
+        fireAnimator.SetBool("spellOver", true);
+        // Destroy fire after 0.32 seconds of fire end animation
+        Destroy(this.gameObject, 0.32f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Deathwalker/Assets/Scripts/WizardController.cs
@@ -0,0 +1,92 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WizardController : Movement
+{
+    public float cooldown = 3.0f;
+    public float lastUsed;
+    public GameObject player;
+    public GameObject spellPrefab;
+    private bool startCasting = false;
+    private bool firstSpellCasted = false;
+    private bool inRange = false;
+    private Animator wizardAnimator;
+
+    // Start is called before the first frame update
+    protected override void Start()
+    {
+        base.Start();
+        StartCoroutine(countdownToCast());
+        wizardAnimator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    protected override void Update()
+    {
+        base.Update();
+        // Get relative direction to player
+        float x = player.transform.position.x - transform.position.x;
+        float y = player.transform.position.y - transform.position.y;        
+        // If player is in casting range and allowed to shoot
+        if (startCasting && inRange) {
+            // Update animation to activate idle
+            wizardAnimator.SetFloat("xySpeed", 0);
+            // If first spell is not yet casted
+            if (!firstSpellCasted) {
+                // Cast spell on player's current position
+                Cast();
+            } 
+            else {
+                // If cooldown is over, cast spell
+                if (Time.time - lastUsed > cooldown) {
+                    Cast(); 
+                }
+            }
+        }
+        // If player is not in casting range
+        if (!inRange) {
+            wizardAnimator.SetFloat("xySpeed", (Mathf.Abs(x) + Mathf.Abs(y)));
+            UpdateMovement(new Vector3(x,y,0));
+        }
+    }
+    void OnTriggerEnter2D(Collider2D col) 
+    {
+        if (col.tag == "Player") {
+            inRange = true;
+        }
+    }
+    void OnTriggerExit2D(Collider2D col)
+    {
+        if (col.tag == "Player") {
+            inRange = false;
+        }
+    }
+    // Wait 1.5 seconds before wizard casts spell when it is spawned
+    IEnumerator countdownToCast() 
+    {
+        yield return new WaitForSeconds(1.5f);
+        // Start casting after waiting for an initial 1.5 seconds
+        startCasting = true;
+    }
+    IEnumerator startCastAnimation() 
+    {
+        // Let animation play for 0.3 seconds
+        yield return new WaitForSeconds(0.3f);
+        // Get player's position
+        Vector3 playerPos = new Vector3(player.transform.position.x, player.transform.position.y, player.transform.position.z);
+        // Spawn spell at player's position
+        GameObject spell = Instantiate(spellPrefab, playerPos, Quaternion.identity);
+        // Set casting to false
+        wizardAnimator.SetBool("casting", false);
+    }
+    void Cast()
+    {   
+        wizardAnimator.SetBool("casting", true);
+        firstSpellCasted = true;
+        // Update last time arrow was shot
+        lastUsed = Time.time;
+        // Start cast animation and instantiate spell prefab
+        StartCoroutine(startCastAnimation());
+    }
+}
