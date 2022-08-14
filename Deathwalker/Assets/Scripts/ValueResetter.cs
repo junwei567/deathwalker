e83f240942d61adae536d2aae408ee0bbfff2c3d
Marcus Ho Jw <marcushojww@gmail.com>
2022-08-15 04:27:36 +0800

    easier settings

--- Deathwalker/Assets/Scripts/ValueResetter.cs
+++ Deathwalker/Assets/Scripts/ValueResetter.cs
@@ -22,9 +22,9 @@ public class ValueResetter : MonoBehaviour
         // Setting default values
         playerHealthSO.Value = 5;
         playerLivesSO.Value = 3;
-        dungeon1Kills.Value = 40;
-        dungeon2Kills.Value = 80;
-        dungeon4Kills.Value = 120;
+        dungeon1Kills.Value = 20;
+        dungeon2Kills.Value = 30;
+        dungeon4Kills.Value = 40;
         currentDungeon.Value = "Dungeon1";
         // Make cursor visible
         Cursor.visible = true;
